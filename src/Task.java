0f750848c15a438c4b89642b225a52b0e57ec82d
Bahram <bahramduvazaev@yandex.ru>
2025-06-08 00:56:39 +0300

    Добавлены новые файлы

--- src/Task.java
+++ src/Task.java
@@ -13,4 +13,27 @@ public class Task {
         this.id = id;
         this.status = status;
     }
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setId(int id) {
+        this.id = id; // Метод добавлен для установки ID задач
+    }
 }
